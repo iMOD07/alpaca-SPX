2652d52ae4d2c780d48c87972b2f44542e78b55a
iMOD07 <mo.2301@hotmail.com>
2025-11-20 12:05:32 +0300

    test / Modify deploy.yml to run JAR files

--- src/main/java/com/mod98/alpaca/spx/SpxApplication.java
+++ src/main/java/com/mod98/alpaca/spx/SpxApplication.java
@@ -17,6 +17,6 @@ public class SpxApplication {
         dotenv.entries().forEach(e -> System.setProperty(e.getKey(), e.getValue()));
 
         SpringApplication.run(SpxApplication.class, args);
-        System.out.println("test add new branch");
+        System.out.println("test / Modify deploy.yml to run JAR files");
     }
 }
